7dc09164d6a62ddfa5672ba7a7d61a15e3b98d08
ykr <r.yeredla@iitg.ac.in>
2025-05-23 00:41:07 +0530

    added patient login under users chema

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,38 @@
 import express from "express";
-import{config} from "dotenv";
+import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import fileUpload from "express-fileupload"
-import {dbConnection} from "./database/dbConnection.js";
-import messageRouter from "./router/messageRouter.js";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js"
+import fileUpload from "express-fileupload";
+
+import { dbConnection } from "./database/dbConnection.js";
+import messageRouter from "./router/messageRouter.js";  // default import
+import userRouter from "./router/userRouter.js";        // fixed import, default import
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+
 const app = express();
-config({path: "./config/config.env"});
+
+config({ path: "./config/config.env" });
+
 app.use(cors({
-    origin: [process.env.FRONTEND_URL,process.env.BACKEND_URL],
+    origin: [process.env.FRONTEND_URL, process.env.BACKEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
+
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(fileUpload({
-    useTemporary: true,
+    useTempFiles: true,       // fixed option name here
     tempFileDir: "/tmp/",
 }));
+
 app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+
 dbConnection();
+
 app.use(errorMiddleware);
+
 export default app;
