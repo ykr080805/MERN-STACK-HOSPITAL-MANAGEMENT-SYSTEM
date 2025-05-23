70e7cd39dac19da2163d5dc15b658abd3c8ceed0
ykr <r.yeredla@iitg.ac.in>
2025-05-23 23:30:44 +0530

    added new functions such as getAllAppointments,updateAppointmentStatus,deleteAppointment which can be done by admin only

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -69,5 +69,44 @@ export const postAppointment = catchAsyncErrors(async(req,res,next)=>{
 res.status(200).json({
     success:true,
     message:"Appointment Sent Successfully",
-})
+});
+});
+
+export const getAllAppointments = catchAsyncErrors(async(req,res,next)=>{
+    const appointments = await Appointment.find();
+    res.status(200).json({
+        success:true,
+        appointments,
+    });
+});
+
+export const updateAppointmentStatus = catchAsyncErrors(async(req,res,next)=>{
+    const {id} = req.params;
+    let appointment = await Appointment.findById(id);
+    if(!appointment){
+        return next(new ErrorHandler("Appointment Not Found",400));
+    }
+    appointment=await Appointment.findByIdAndUpdate(id,req.body,{
+        new : true,
+        runValidators : true,
+        useFindAndModify : false,
+    });
+    res.status(200).json({
+        success:true,
+        message:"Appointment Status Updated",
+        appointment,
+    });
+});
+
+export const deleteAppointment = catchAsyncErrors(async(req,res,next)=>{
+    const {id} = req.params;
+    let appointment = await Appointment.findById(id);
+    if(!appointment){
+        return next(new ErrorHandler("Appointment Not Found",400));
+    }
+    await appointment.deleteOne();
+    res.status(200).json({
+        success:true,
+        message:"Appointment Deleted",
+    })
 });
--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,6 +1,9 @@
 import express from "express"
-import { postAppointment } from "../controller/appointmentController.js";
+import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
 import {isAdminAuthenticated,isPatientAuthenticated} from "../middlewares/auth.js"
 const router = express.Router();
 router.post("/post",isPatientAuthenticated,postAppointment);
+router.get("/getall",isAdminAuthenticated,getAllAppointments);
+router.put("/update/:id",isAdminAuthenticated,updateAppointmentStatus);
+router.delete("/delete/:id",isAdminAuthenticated,deleteAppointment);
 export default router;
