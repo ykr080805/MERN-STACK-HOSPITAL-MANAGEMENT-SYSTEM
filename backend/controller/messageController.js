2d3eadfd3a4a2b57b1ffe7bcb86387a560ca4a28
dhanushsai2006 <dhanushsai83@gmail.com>
2025-05-28 20:06:03 +0530

    Debuged error in messageController

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -4,7 +4,7 @@ import ErrorHandler from "../middlewares/errorMiddleware.js"
 export const sendMessage = catchAsyncErrors(async (req,res,next) => {
     const {firstName, lastName, email, phone, message} = req.body;
     if (!firstName || !lastName || !email || !phone || !message) {
-        return next(new ErrorHandler("Please Fill Full Form"),400)
+        return next(new ErrorHandler("Please Fill Full Form",400))
     }
         await Message.create({firstName, lastName, email, phone, message});
         res.status(200).json({
